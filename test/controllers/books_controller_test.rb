ccdef279984bc14fa6ed51bf3aaf849c2ddc3e3c
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-09-14 13:01:58 +0000

    [Finish]task2

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,28 +1,28 @@
 require "test_helper"
 
-class HomesControllerTest < ActionDispatch::IntegrationTest
+class BooksControllerTest < ActionDispatch::IntegrationTest
   test "should get top" do
-    get homes_top_url
+    get books_top_url
     assert_response :success
   end
 
   test "should get new" do
-    get homes_new_url
+    get books_new_url
     assert_response :success
   end
 
   test "should get index" do
-    get homes_index_url
+    get books_index_url
     assert_response :success
   end
 
   test "should get edit" do
-    get homes_edit_url
+    get books_edit_url
     assert_response :success
   end
 
   test "should get show" do
-    get homes_show_url
+    get books_show_url
     assert_response :success
   end
 end
