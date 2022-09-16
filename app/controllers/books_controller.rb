dedf9e5a2284708b849c0526ab1871240fcb30d0
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-09-16 13:12:47 +0000

    [Finish]task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,15 @@
 class BooksController < ApplicationController
 
   def new
-    @book = Book.new
+
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/top'
+
   end
 
   def index
-    @book = Book.find(book_params)
+
   end
 
   def edit
@@ -19,10 +17,7 @@ class BooksController < ApplicationController
 
   def show
   end
-
- private
-
- def book_params
-  params.require(:book).permit(title:本のタイトル, body:感想)
- end
 end
+
+
+
