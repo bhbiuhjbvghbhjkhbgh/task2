ed8bf41e4d462ee2a98113e3ba107a35878120e6
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-09-20 13:21:36 +0000

    [Finish]task2]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,8 +19,20 @@ class BooksController < ApplicationController
        @book = Book.find(params[:id])
   end
 
+  def update
+      book = Book.find(params[:id])
+      book.update(book_params)
+      redirect_to book_path(book.id)
+  end
+
   def show
      @book = Book.find(params[:id])
+  end
+
+  def destroy
+     book = Book.find(params[:id])
+     book.destroy
+     redirect_to'/books'
   end
 
   private
